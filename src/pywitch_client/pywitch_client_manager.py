de6c75f950b411a59b8c1dab5ba9a365e5c5912c
Gustavo <ourique.gustavo@gmail.com>
2021-11-07 00:06:46 -0300

    Ready for tests!

--- src/pywitch_client/pywitch_client_manager.py
+++ src/pywitch_client/pywitch_client_manager.py
@@ -1,20 +1,61 @@
 import json
+import time
 import string
 import random
 import requests
 import threading
-from pywitch import validate_token
 from flask import Flask, request
+from pywitch import (
+    validate_token,
+    get_user_info,
+    PyWitchTMI,
+    PyWitchHeat,
+    PyWitchRedemptions,
+    PyWitchStreamInfo,
+)
 
-events={
+valid_char = string.ascii_letters + string.digits
+
+
+def random_string(length):
+    return ''.join([random.choice(valid_char) for i in range(length)])
+
+
+events = {
     'tmi': {},
     'heat': {},
     'redemptions': {},
-    'stream_info': {},
-
+    'streaminfo': {},
 }
 
+
+def tmi_callback(data):
+    data.pop('event_raw')
+    data['pywitch_id'] = random_string(64)
+    print(data)
+    events['tmi'] = json.dumps(data, ensure_ascii=False)
+
+
+def heat_callback(data):
+    data.pop('event_raw')
+    data['pywitch_id'] = random_string(64)
+    events['heat'] = json.dumps(data, ensure_ascii=False)
+
+
+def redemptions_callback(data):
+    data.pop('event_raw')
+    data['pywitch_id'] = random_string(64)
+    events['redemptions'] = json.dumps(data, ensure_ascii=False)
+
+
+def streaminfo_callback(data):
+    data.pop('event_raw')
+    data['pywitch_id'] = random_string(64)
+    events['streaminfo'] = json.dumps(data, ensure_ascii=False)
+
+
 app = Flask('app')
+
 
 @app.route('/<kind>')
 def route_tmi(kind):
@@ -22,10 +63,11 @@ def route_tmi(kind):
         return '{}'
     return json.dumps(events[kind], ensure_ascii=False)
 
+
 class PyWitchClientManager:
     def __init__(
         self,
-        token = None,
+        token=None,
         host='127.0.0.1',
         port=13486,
         auth_server='https://pywitch-auth.herokuapp.com',
@@ -45,23 +87,19 @@ class PyWitchClientManager:
         self.auth_client_id = auth_client_id
         self.auth_scopes = auth_scopes
 
+        self.features = {}
+
         if self.port:
             self.server_url = f'{self.host}:{self.port}'
         else:
             self.server_url = self.host
-            
+
         self.is_running = False
-        
+
     def get_auth_url(self, auth_endpoint='authenticate'):
         self.auth_endpoint = auth_endpoint
-        self.auth_state_valid = string.ascii_letters + string.digits
-        self.auth_state_lenght = 128
-        self.auth_state = ''.join(
-            [
-                random.choice(self.auth_state_valid)
-                for i in range(self.auth_state_lenght)
-            ]
-        )
+        self.auth_state_length = 128
+        self.auth_state = random_string(self.auth_state_length)
         self.auth_scopes_str = '%20'.join(self.auth_scopes)
         self.auth_url = (
             'https://id.twitch.tv/oauth2/authorize'
@@ -88,7 +126,10 @@ class PyWitchClientManager:
         except:
             self.validation = {}
         return self.validation
-        
+
+    def verify_channel(self, channel):
+        return get_user_info(login=channel, helix_headers=self.helix_headers)
+
     def start_flask(self):
         kwargs = {
             'host': self.host,
@@ -98,8 +139,60 @@ class PyWitchClientManager:
             'debug': False,
         }
 
-        self.thread = threading.Thread(
+        self.flask_thread = threading.Thread(
             target=app.run, daemon=True, kwargs=kwargs
         )
-        self.thread.start()
-        
+        self.flask_thread.start()
+
+    def start_validator(self, token):
+        self.is_running = True
+        self.token = token
+        self.validator_thread = threading.Thread(
+            target=self.validator_task, args=(self.token,), daemon=True
+        )
+        self.validator_thread.start()
+
+    def validator_task(self, token, interval=600):
+        time.sleep(interval)
+        num = 0
+        while self.is_running:
+            # The pourpose behind this is to avoid multiple instances of
+            # validator running at the same time.
+            num += 1
+            time.sleep(1)
+            if num >= interval:
+                self.validate(token)
+
+    def start_pywitch(self, channel, token, features):
+        self.is_running = True
+        self.token = token
+        self.channel = channel
+        if not self.channel:
+            raise Exception("Missing channel!")
+        if not self.token:
+            raise Exception("Missing token!")
+        if 'tmi' in features:
+            self.features['tmi'] = PyWitchTMI(
+                self.channel, self.token, tmi_callback
+            )
+        if 'heat' in features:
+            self.features['heat'] = PyWitchHeat(
+                self.channel, self.token, heat_callback
+            )
+        if 'redemptions' in features:
+            self.features['redemptions'] = PyWitchRedemptions(
+                self.token, redemptions_callback
+            )
+        if 'streaminfo' in features:
+            self.features['streaminfo'] = PyWitchStreamInfo(
+                self.channel, self.token, streaminfo_callback
+            )
+
+        for v in self.features.values():
+            v.start()
+
+    def stop_all(self):
+        self.is_running = False
+        for v in self.features.values():
+            thread = threading.Thread(target=v.stop, args=(), daemon=True)
+            thread.start()
--- src/pywitch_client/pywitch_client_window.py
+++ src/pywitch_client/pywitch_client_window.py
@@ -1,15 +1,20 @@
 from PyQt5.QtWidgets import (
     QLabel,
     QVBoxLayout,
+    QHBoxLayout,
     QMainWindow,
     QApplication,
     QWidget,
     QPushButton,
     QCheckBox,
+    QLineEdit,
+    QFormLayout,
+    QMessageBox,
 )
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import QPoint
-import zlib
+from PyQt5.QtCore import QPoint, Qt
+import os
+import sys
 import time
 import string
 import random
@@ -19,58 +24,87 @@ from os.path import isfile, join
 from configparser import ConfigParser
 from pywitch_client_manager import PyWitchClientManager
 
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 
 class PyWitchClientWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(PyWitchClientWindow, self).__init__(*args, **kwargs)
         self.setWindowTitle("PyWitch Client")
         self.setFixedWidth(225)
-        
+
         self.read_config()
         self.token = self.config['authentication']['token']
-        
-        self.manager = PyWitchClientManager(self.token)
+        self.channel = self.config['authentication']['channel']
+        self.host = self.config['connection']['host']
+        self.port = self.config['connection']['port']
+
+        self.manager = PyWitchClientManager(self.token, self.host, self.port)
+        self.manager.start_flask()
+
         self.auth_url, self.auth_state = self.manager.get_auth_url()
-        print(self.auth_url)
-        
+
         self.main_widget = QWidget()
         self.layout = QVBoxLayout()
         self.main_widget.setLayout(self.layout)
-        
-        self.setup_button()        
+
+        self.setup_button()
+        self.setup_writebox()
         self.setup_toggle_box()
 
         self.setCentralWidget(self.main_widget)
-        
-##############################################################################
+
+    ##########################################################################
+
+    def reset_widgets(self):
+        self.button_widget.setText('Start PyWitch Server')
+        self.button_widget.setEnabled(True)
+        self.writebox_widget.setEnabled(True)
+        for v in self.opt_check.values():
+            v.setEnabled(True)
+        self.image.setEnabled(True)
+
+    ##########################################################################
 
     def setup_button(self):
         self.button_widget = QPushButton()
         self.button_widget.setText('Start PyWitch Server')
         self.button_widget.clicked.connect(self.click_button)
         self.layout.addWidget(self.button_widget)
-        
+
     def click_button(self):
         self.button_widget.setEnabled(False)
+        self.writebox_widget.setEnabled(False)
         for v in self.opt_check.values():
             v.setEnabled(False)
+        self.image.setEnabled(False)
         if not self.manager.is_running:
             if self.token:
                 self.button_widget.setText('Validating token...')
-        thread=threading.Thread(target=self.button_clicked_thread, args=())
+        thread = threading.Thread(target=self.button_clicked_thread, args=())
         thread.start()
-                
+
     def button_clicked_thread(self):
         if not self.manager.is_running:
             validation = self.manager.validate(self.token)
             if not self.token:
                 self.button_widget.setText('Waiting for authorization...')
                 webbrowser.open(self.auth_url)
-            thread=threading.Thread(target=self.watch_for_token, args=())
+            thread = threading.Thread(target=self.watch_for_token, args=())
             thread.start()
-            
-    def watch_for_token(self, endpoint='state', interval = 3, max_tries=60):
-        num=0
+        else:
+            self.button_widget.setText('Stopping Pywitch Server...')
+            self.manager.stop_all()
+            self.reset_widgets()
+
+    def watch_for_token(self, endpoint='state', interval=3, max_tries=60):
+        num = 0
         while num < max_tries and not self.token:
             time.sleep(interval)
             num += 1
@@ -78,11 +112,59 @@ class PyWitchClientWindow(QMainWindow):
         self.config['authentication']['token'] = self.token
         self.save_config()
         self.button_widget.setText('Starting PyWitch Server...')
-        self.manager.start_flask()
-            
-        
-##############################################################################
-        
+        self.manager.validate(self.token)
+        if not self.manager.validation:
+            self.popup_error('Invalid token!')
+            return
+        self.channel_data = self.manager.verify_channel(self.channel)
+        if not self.channel_data['login']:
+            self.popup_error('Invalid channel!')
+            return
+
+        self.features = [k for k, v in self.opt_check.items() if v.isChecked()]
+        self.manager.start_validator(self.token)
+        self.manager.start_pywitch(self.channel, self.token, self.features)
+        self.button_widget.setText('PyWitch Server Running!')
+        self.button_widget.setEnabled(True)
+
+    ##########################################################################
+
+    def popup_error(self, msg):
+        self.popup = PyWitchClientPopupError(msg, self.close_event)
+        self.popup.button.clicked.connect(self.close_popup)
+        self.popup.show()
+
+    def close_popup(self):
+        self.popup.close()
+
+    def close_event(self):
+        self.reset_widgets()
+
+    ##########################################################################
+
+    def setup_writebox(self):
+        self.writebox_widget = QWidget()
+        self.writebox_layout = QFormLayout()
+        self.writebox_widget.setLayout(self.writebox_layout)
+
+        self.wb_widget = QLineEdit()
+        self.wb_widget.setText(self.channel)
+        self.wb_widget.textChanged.connect(self.wb_changed)
+
+        self.writebox_layout.addRow('Channel:', self.wb_widget)
+
+        self.layout.addWidget(self.writebox_widget)
+
+    def wb_changed(self, text):
+        self.config['authentication']['channel'] = text
+        self.channel = self.config['authentication']['channel']
+        if not text:
+            self.button_widget.setEnabled(False)
+        else:
+            self.button_widget.setEnabled(True)
+
+    ##########################################################################
+
     def setup_toggle_box(self):
         self.opt_widget = QWidget()
         self.opt_layout = QVBoxLayout()
@@ -94,10 +176,10 @@ class PyWitchClientWindow(QMainWindow):
             'stream_info': QCheckBox("Stream Information"),
         }
         for k, v in self.opt_check.items():
-            v.setChecked(self.config.getboolean('features',k))
+            v.setChecked(self.config.getboolean('features', k))
             v.setToolTip(f'{self.manager.server_url}/{k}')
             self.opt_layout.addWidget(v)
-        
+
         self.opt_check['tmi'].toggled.connect(
             lambda: self.toggle_feature('tmi')
         )
@@ -111,34 +193,44 @@ class PyWitchClientWindow(QMainWindow):
             lambda: self.toggle_feature('stream_info')
         )
         self.layout.addWidget(self.opt_widget)
-        
-        pixmap = QPixmap(join('logo','pywitch_logo.png'))
+
+        img_path = resource_path(join('logo', 'pywitch_logo.png'))
+        print( img_path )
+
+        pixmap = QPixmap(img_path)
         self.image = QLabel(self.opt_widget)
         self.image.setPixmap(pixmap)
         self.image.setFixedSize(pixmap.size())
         p = QPoint(130, 00)
         self.image.move(p)
-        
-    def toggle_feature(self,kind):
-        state=self.opt_check[kind].isChecked()
+
+    def toggle_feature(self, kind):
+        state = self.opt_check[kind].isChecked()
         self.config['features'][kind] = str(state)
         self.save_config()
-        
-##############################################################################
-        
+
+    ##########################################################################
+
+    def closeEvent(self, event):
+        self.save_config()
+        event.accept()
+
+    ##########################################################################
+
     def save_config(self):
-        if not hasattr(self,'config'):
+        if not hasattr(self, 'config'):
             self.read_config()
         with open(self.config_file, 'w') as open_cf:
             self.config.write(open_cf)
-        
-    def read_config(self, config_file = 'pywitch_client_config.ini'):
+
+    def read_config(self, config_file='pywitch_client_config.ini'):
         self.config_file = config_file
         self.config = ConfigParser()
         if isfile(self.config_file):
             self.config.read(config_file)
-        else:            
-            self.config['authentication'] = {'token': ''}
+        else:
+            self.config['authentication'] = {'token': '', 'channel': 'gleenus'}
+            self.config['connection'] = {'host': '127.0.0.1', 'port': 13486}
             self.config['features'] = {
                 'tmi': True,
                 'heat': True,
@@ -147,6 +239,24 @@ class PyWitchClientWindow(QMainWindow):
             }
             with open(self.config_file, 'w') as open_cf:
                 self.config.write(open_cf)
-            
-            
-            
+
+
+    ##########################################################################
+
+
+class PyWitchClientPopupError(QMainWindow):
+    def __init__(self, msg, close_event):
+        super().__init__()
+
+        self.setWindowTitle('Error')
+        self.close_event = close_event
+
+        self.widget = QMessageBox()
+        self.widget.setIcon(QMessageBox.Critical)
+        self.widget.setText('Error!')
+        self.widget.setInformativeText(msg)
+        self.button = self.widget.addButton(QMessageBox.Ok)
+        self.setCentralWidget(self.widget)
+
+    def closeEvent(self, event):
+        self.close_event()
